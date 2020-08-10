5a3b772756395ef273c54b99423197aa62c7a772
Johan Back <johan.backs@gmail.com>
2020-08-10 14:59:34 -0300

    add server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
-const app =  express();
+import routes from './routes'
+
+const app = express();
 
 app.use(express.json());
+app.use(routes)
 
-app.get('/',(request,response) => {
-  return response.json({message: "Hello"})
-})
 
-app.listen(3030);
+app.listen(3030)
